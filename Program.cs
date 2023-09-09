9c437c94b4eb976fc6f6cf70df4a137ba8e04f41
jamesjavanyc` <1931736857@qq.com>
2023-09-09 12:55:30 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace MyFirstConsoleApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string yourName = "James";
+            string yourLocation = "NY";
+
+            Console.WriteLine($"My name is {yourName}, and I'm from {yourLocation}");
+
+            DateTime currentDate = DateTime.Now;
+
+            Console.WriteLine($"Today's date is {currentDate.ToString("M / d / yyyy")}");
+
+            DateTime christmasDate = new DateTime(currentDate.Year, 12, 25);
+            TimeSpan daysUntilChristmas = christmasDate - currentDate;
+            int days = (int)daysUntilChristmas.TotalDays;
+
+            Console.WriteLine($"Days left until Christmas this year: {days} days");
+        }
+    }
+}
