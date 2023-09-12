b2c3acd0cbb74d110dccf622bfc9d28ed32c31c9
jamesjavanyc` <1931736857@qq.com>
2023-09-11 23:49:58 -0400

    Commit the combined code

--- Program.cs
+++ Program.cs
@@ -19,6 +19,23 @@
 
             Console.WriteLine($"Days left until Christmas this year: {days} days");
 
+            double width, height, woodLength, glassArea;
+            string widthString, heightString;
+
+            Console.Write("Width Input: ");
+            widthString = Console.ReadLine();
+            width = double.Parse(widthString);
+
+            Console.Write("Height Input: ");
+            heightString = Console.ReadLine();
+            height = double.Parse(heightString);
+
+            woodLength = 2 * (width + height) * 3.25;
+            glassArea = 2 * (width * height);
+
+            Console.WriteLine("The length of the wood is " + woodLength + " feet");
+            Console.WriteLine("The area of the glass is " + glassArea + " square metres");
+
             Console.ReadKey();
         }
     }
